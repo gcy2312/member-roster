4497b63576088c45d634ea2c2b1138b8440727db
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2022-01-27 16:36:11 -0500

    mock data

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const mockData = {
+  "members": [
+    { id: 1, firstName: "Suzie", lastName: "Jones", age: "27", rating: 7, activities: ["Hiking", "Running", "Biking"] },
+    { id: 2, firstName: "Tom", lastName: "Holmes", age: "33", rating: 9, activities: ["Biking", "Skiing"] },
+    { id: 3, firstName: "Jenny", lastName: "Turner", age: "23", rating: 6, activities: ["Biking", "Skiing", "Swimming"] },
+    { id: 4, firstName: "Thom", lastName: "Davis", age: "25", rating: 9, activities: ["Skiing", "Swimming", "Hiking"] },
+    { id: 5, firstName: "David", lastName: "Roberts", age: "35", rating: 10, activities: ["Biking", "Skiing", "Climbing"] },
+    { id: 6, firstName: "Lisa", lastName: "Monroe", age: "27", rating: 6, activities: ["Biking", "Swimming"] },
+  ]
+}
